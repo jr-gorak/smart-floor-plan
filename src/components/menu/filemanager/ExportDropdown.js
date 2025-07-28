3973f2b89d1354b25ba952b9de972c4fee023abf
jr-gorak <jkg11@aber.ac.uk>
2025-07-28 22:42:08 +0100

    Generating images and zip files
    Wrote code that generates a PNG for each floor as an image (hiding devices), storing them in a zip file. If user is choosing to export for home assistant, it will await JSON file generation and canvas image generation before creating the downloaded zip file. If a user is only exporting a singular file, it should save as a normal png.

--- src/components/menu/filemanager/ExportDropdown.js
+++ src/components/menu/filemanager/ExportDropdown.js
@@ -1,6 +1,8 @@
 import '../../css/Dropdown.css';
 import { useState } from 'react';
 import * as fabric from "fabric";
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 function ExportDropdown({activeDropdown}) {
   const [floorData, setFloorData] = useState(() => {const stored = sessionStorage.getItem("floorData"); return stored? JSON.parse(stored) : {}; });
@@ -11,164 +13,209 @@ function ExportDropdown({activeDropdown}) {
   const canvasWidth = sessionStorage.getItem('canvasWidth');
   const canvasHeight = sessionStorage.getItem('canvasHeight');
   const canvasName = sessionStorage.getItem('canvasName');
-  //What data would I need?
-  // floordata
-  //Can get: floor registry: floor name, id, etc.
-  //Can get: canvas objects, meaning classified='mark', to generate area registry.
+  const deviceList = JSON.parse(sessionStorage.getItem('sensors'));
+  var zip = new JSZip();
 
-  //device list
-  //This list of objects is what is necessary to update entity and device files.
+  async function generateImages(purpose) {
+    const dataLength = Object.keys(floorData).length;
+    let processedFiles = 1;
 
-  //original config files
-  //These will be modified. 
+    return new Promise((resolve) => {
+      Object.entries(floorData).forEach(([key, data]) => {
+        let finishedDownloading = false;
 
-  //LabelList
-  //Used to generate label registry
+        const canvas = new fabric.Canvas(null, {
+          width: canvasWidth,
+          height: canvasHeight,
+          backgroundColor: "white",
+        });
 
-  function generateImages() {
-    for (const key in floorData) {
-      const data = floorData[key];
+        canvas.loadFromJSON(data, () => {
+          if (finishedDownloading) return;
+          setTimeout(() => {
+            canvas.getObjects().filter(obj => obj.classifier === 'device' || obj.classifier === 'sensor').forEach(obj => {
+              obj.visible = false;
+              console.log(obj);
+            })
+          }, 0)
 
-      let finishedDownloading = false;
+          canvas.renderAll();
 
-      const canvas = new fabric.Canvas(null, {
-        width: canvasWidth,
-        height: canvasHeight,
-        backgroundColor: "white",
-      });
+          if (dataLength === 1) {
+            setTimeout(() => {
+              const floorplanPNG = canvas.toDataURL({format: 'png'})
+              const link = document.createElement('a');
+              link.href = floorplanPNG;
+              link.download = canvasName + '_' + key + '.png';
+              link.click();
+            }, 0)
+          } else if (dataLength > 1) {
+            setTimeout(() => {
+              const floorplanPNG = canvas.toDataURL({format: 'png'})
+              const fileName =  canvasName + '_' + key + '.png';
+              zip.folder('media').file(fileName, floorplanPNG.split(',')[1], {base64: true})
+              processedFiles++
+              console.log(processedFiles);
 
-      canvas.loadFromJSON(data, () => {
-        if (finishedDownloading) return;
+              if (processedFiles === dataLength && purpose === 'images') {
+                zip.generateAsync({type: 'blob'}).then((blob) => {
+                  saveAs(blob, 'image_export.zip')
+                  zip = new JSZip();
+                  resolve();
+                });
 
-        setTimeout(() => {
-          canvas.getObjects().filter(obj => obj.classifier === 'device' || obj.classifier === 'sensor').forEach(obj => {
-            obj.visible = false;
-            console.log(obj);
-          })
-        }, 0)
-
-        canvas.renderAll();
-
-        setTimeout(() => {
-          const floorplanPNG = canvas.toDataURL({format: 'png'})
-          const link = document.createElement('a');
-          link.href = floorplanPNG;
-          link.download = canvasName + '_' + key + '.png';
-          link.click();
-        }, 0)
-
-      finishedDownloading = true;
+              } else if (processedFiles === dataLength && purpose === 'home-assistant') {
+                console.log("test")
+                resolve();
+              }
+                
+            }, 0)
+          }
+          finishedDownloading = true;
+        })
+      })
     })
-    }
-    
   }
 
-  function exportData() {
-
-  function calculateLevel(floor) {
-    if (floor.includes("GR")) {
-      return 0
-    } else if (floor.includes("F")) {
-      return parseInt(floor.slice(0));
-    } else if (floor.includes("B")) {
-      return -parseInt(floor.slice(0));
-    }
-  }
-
-  function generateFloorData() {
-    const dataArray = [];
-
-    for (const i in floorArray) {
-      const floorData = {
-        aliases: [],
-        floor_id: floorArray[i],
-        icon: null,
-        level: calculateLevel(floorArray[i]),
-        name: floorArray[i],
-        created_at: Date(),
-        modified_at: Date(),
+  async function generateJsonFiles() {
+    function calculateLevel(floor) {
+      if (floor.includes("GR")) {
+        return 0
+      } else if (floor.includes("F")) {
+        return parseInt(floor.slice(0));
+      } else if (floor.includes("B")) {
+        return -parseInt(floor.slice(0));
       }
-      dataArray.push(floorData)
+    }
+
+    function generateFloorData() {
+      const dataArray = [];
+
+      for (const i in floorArray) {
+        const floorData = {
+          aliases: [],
+          floor_id: floorArray[i],
+          icon: null,
+          level: calculateLevel(floorArray[i]),
+          name: floorArray[i],
+          created_at: Date(),
+          modified_at: Date(),
+        }
+        dataArray.push(floorData)
+        }
+        return dataArray;
+      }
+
+    function generateAreaData() {
+      const dataArray = [];
+
+      for (const key in floorData) {
+        const data = floorData[key];
+        const areaData = data.objects.filter(obj => obj.classifier === 'mark').map(obj => ({
+          aliases: [],
+          floor_id: key,
+          humidity_entity_id: null,
+          icon: null,
+          id: obj.area_id,
+          labels: [],
+          name: obj.area_id,
+          picture: null,
+          temperature_entity_id: null,
+          created_at: Date(),
+          modified_at: Date(),
+        }))
+        dataArray.push(...areaData);
       }
       return dataArray;
     }
 
-  function generateAreaData() {
-    const dataArray = [];
+    function generateLabelData() {
+      const dataArray = [];
 
-    for (const key in floorData) {
-      const data = floorData[key];
-      const areaData = data.objects.filter(obj => obj.classifier === 'mark').map(obj => ({
-        aliases: [],
-        floor_id: key,
-        humidity_entity_id: null,
-        icon: null,
-        id: obj.area_id,
-        labels: [],
-        name: obj.area_id,
-        picture: null,
-        temperature_entity_id: null,
-        created_at: Date(),
-        modified_at: Date(),
-      }))
-      dataArray.push(...areaData);
+      for (const i in labelList) {
+        if (labelList[i] !== "") {
+          const labelData = {
+            color: null,
+            description: null,
+            icon: null,
+            label_id: labelList[i].toLowerCase().replace(/ /g, '_'),
+            name: labelList[i],
+            created_at: Date(),
+            modified_at: Date(),
+          }
+          dataArray.push(labelData)
+        } 
+      }
+      return dataArray;
     }
-    return dataArray;
-  }
 
-  function generateLabelData() {
-    const dataArray = [];
+    function updateRegistries() {
+      for (const device in deviceList) {
+        const deviceData = deviceList[device]
+        const deviceId = deviceData.id;
+        const coreDevice = deviceRegistry.data.devices.find(device => device.id === deviceId)
+        coreDevice.area_id = deviceData.area_id;
+        coreDevice.name_by_user = deviceData.name;
 
-    for (const i in labelList) {
-      if (labelList[i] !== "") {
-        const labelData = {
-          color: null,
-          description: null,
-          icon: null,
-          label_id: labelList[i].toLowerCase().replace(/ /g, '_'),
-          name: labelList[i],
-          created_at: Date(),
-          modified_at: Date(),
+        for (const entity in deviceData.entities) {
+          const entityData = deviceData.entities[entity]
+          const entityId = entityData.id;
+          const coreEntity = entityRegistry.data.entities.find(entity => entity.id === entityId)
+          coreEntity.area_id = deviceData.area_id;
+          coreEntity.labels.push(entityData.label);
+          coreEntity.name = entityData.name;
         }
-        dataArray.push(labelData)
-      } 
-    }
-    return dataArray;
-  }
-
-  const floorRegistryExport = {
-    version: 1,
-    minor_version: 2,
-    key: "core.floor_registry",
-    data: {
-      floors: generateFloorData()
-    }
-  }
-
-  const areaRegistryExport = {
-    version: 1,
-    minor_version: 8,
-    key: "core.area_registry",
-    data: {
-      areas: generateAreaData()
-    }
-  }
-
-  const labelRegistryExport = {
-    version: 1,
-    minor_version: 2,
-    key: "core.label_registry",
-    data: {
-      labels: generateLabelData()
+      }
     }
 
+    const floorRegistryExport = {
+      version: 1,
+      minor_version: 2,
+      key: "core.floor_registry",
+      data: {
+        floors: generateFloorData()
+      }
+    }
+
+    const areaRegistryExport = {
+      version: 1,
+      minor_version: 8,
+      key: "core.area_registry",
+      data: {
+        areas: generateAreaData()
+      }
+    }
+
+    const labelRegistryExport = {
+      version: 1,
+      minor_version: 2,
+      key: "core.label_registry",
+      data: {
+        labels: generateLabelData()
+      }
+
+    }
+
+    updateRegistries();
+
+    zip.folder('core-registry-files').file('core.floor_registry', JSON.stringify(floorRegistryExport, null, 2))
+    zip.folder('core-registry-files').file('core.area_registry', JSON.stringify(areaRegistryExport, null, 2))
+    zip.folder('core-registry-files').file('core.label_registry', JSON.stringify(labelRegistryExport, null, 2))
+    zip.folder('core-registry-files').file('core.device_registry', JSON.stringify(deviceRegistry, null, 2))
+    zip.folder('core-registry-files').file('core.entity_registry', JSON.stringify(entityRegistry, null, 2))
   }
 
-    console.log("floor registry:", floorRegistryExport)
-    console.log("area registry:", areaRegistryExport)
-    console.log("label registry:", labelRegistryExport)
-    console.log(deviceRegistry);
-    console.log(entityRegistry);
+  async function exportData() {
+
+    await generateImages("home-assistant");
+    await generateJsonFiles();
+
+    await zip.generateAsync({type: 'blob'}).then((blob) => {
+        saveAs(blob, 'file_export.zip')
+        zip = new JSZip();
+    });
+
+
   }
 
   return (
@@ -177,7 +224,7 @@ function ExportDropdown({activeDropdown}) {
     <div className="dropdown-container">
         <div className='dropdown-content'>
         <button onClick={() => exportData()}>Export to Home Assistant</button>
-        <button onClick={() => generateImages()}>Export as png</button>
+        <button onClick={() => generateImages("images")}>Export as png</button>
         </div>
     </div>
     }
