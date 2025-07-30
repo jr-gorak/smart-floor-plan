675382c7085cf5579ee570e7407d39b40b21774e
jr-gorak <jkg11@aber.ac.uk>
2025-07-30 23:57:14 +0100

    Step tabs in guide
    In an effort to approach a product for user-testing, the user guide will be made visually digestible. Easy to click tabs to segregate the steps of the process are created.

--- src/components/menu/information/GuidePopup.js
+++ src/components/menu/information/GuidePopup.js
@@ -1,60 +1,95 @@
 import '../../css/Popup.css';
+import {useState} from 'react'
 
 function Guide({ onClose }) {
-  return (
-    <div className="filter" onClick={onClose}>
-      <div className="frame" onClick={e => e.stopPropagation()}>
 
+  const [tabMode, setTabMode] = useState("home")
+
+  function togglePopup() {
+    onClose();
+    setTabMode("home")
+  }
+
+  return (
+    <div className="filter" onClick={() => togglePopup()}>
+      <div className="guide-frame" onClick={e => e.stopPropagation()}>
         <div className='exit'>
-          <button onClick={onClose}>X</button>
+          <button onClick={() => togglePopup()}>X</button>
         </div>
 
         <h2>How to Use</h2>
-        
-        <div className='popup-content'>
-          <p>This guide will be a brief overview on how to use this application with the purpose of creating a floorplan, adding sensors, and exporting to Home Assistant. </p>
 
-          <p>Please note, a more detailed guide with visual aids will be supplied at a later date.</p>
-          <b>Step 1: Create the Floorplan</b>
-          <p>You will have the option to either upload an image or design a floorplan by scratch.
-          </p>
+        <div className='guide-container'>
+          <div className='tab-menu'>
+            <button className={tabMode === 'home' ? 'button-on' : null} onClick={() => setTabMode('home')}>Home</button>
+            <button className={tabMode === 'step1' ? 'button-on' : null} onClick={() => setTabMode('step1')}>Step 1: Create a Floorplan</button>
+            <button className={tabMode === 'step2' ? 'button-on' : null} onClick={() => setTabMode('step2')}>Step 2: Integrate Sensors</button>
+            <button className={tabMode === 'step3' ? 'button-on' : null} onClick={() => setTabMode('step3')}>Step 3: Export Floorplan</button>
+            <button></button>
+          </div>
+          
+          <div className='popup-content'>
 
-          <b>Create a Canvas</b>
+            {tabMode === 'home' && 
+              <div>
+                <b>How to Use Guide</b>
+                <p>This guide will be a brief overview on how to use this application with the purpose of creating a floorplan, adding sensors, and exporting to Home Assistant. </p>
+                <p>Please note, a more detailed guide with visual aids will be supplied at a later date.</p>
+              </div>
+            }
 
-          <p>You can create a canvas by selecting the "create new" button. This will prompt you to create 
-          a new base canvas, or upload an image as the base for a canvas. You enter your desired dimensions, name the floorplan, and then you can begin creating!</p>
+            {tabMode === 'step1' && 
+              <div>
+                <b>Step 1: Create the Floorplan</b>
+                <p>You will have the option to either upload an image or design a floorplan by scratch.
+                </p>
 
-          <b> Creating a Floor Plan </b>
+                <b>Create a Canvas</b>
 
-          <p> The left side of the menu contains the toolset necessary to create a floor plan. Each button will create a popup menu with different functionalities. </p>
+                <p>You can create a canvas by selecting the "create new" button. This will prompt you to create 
+                a new base canvas, or upload an image as the base for a canvas. You enter your desired dimensions, name the floorplan, and then you can begin creating!</p>
 
-          <b>Drawing</b>
-          <p> With the draw menu, you have the options to create lines, rectangles, circles, and marking rooms. These will be the foundations to your floorplan. This will allow you to create the different walls for the home.
-            Through marking rooms, you simply click the "mark rooms" button and begin tracing the rooms you wish to label. On successful tracing, the room will be colored red and given a room label.
-            You can configure the color of the room and it's designated label. </p>
+                <b> Creating a Floor Plan </b>
 
-          <b>Components</b>
-          <p> The components menu contains any miscellaneous elements necessary for building a floor
-          plan. This contains doorways, windows, stairways, and furniture </p>
+                <p> The left side of the menu contains the toolset necessary to create a floor plan. Each button will create a popup menu with different functionalities. </p>
 
-          <b>Step 2: Integrate your sensors</b>
+                <b>Drawing</b>
+                <p> With the draw menu, you have the options to create lines, rectangles, circles, and marking rooms. These will be the foundations to your floorplan. This will allow you to create the different walls for the home.
+                  Through marking rooms, you simply click the "mark rooms" button and begin tracing the rooms you wish to label. On successful tracing, the room will be colored red and given a room label.
+                  You can configure the color of the room and it's designated label. </p>
 
-          <p> To generate a sensor menu, you must have integrated your desired sensors into your Home Assistant software. This software can generate the configuration files necessary for developing your list of sensors.
-            A more detailed guide will come later in regards towards developing these lists and files. Once acquired, upload the files into this application and a list will be generated. </p>
+                <b>Components</b>
+                <p> The components menu contains any miscellaneous elements necessary for building a floor
+                plan. This contains doorways, windows, stairways, and furniture </p>
+              </div> 
+            }
 
-          <p> To place sensors, you click on the device you wish to place. This will open the device settings screen. Here, you can change the device display name, and edit which sensors you wish to focus on and view in the
-            Home Assistant dashboard upon exporting. For example, you may have a multisensor with several types of sensors. Although, your usability only requires one of the sensors. Simply select to make that sensor active. 
-            When creating the device, it will appear on the floorplan. You can drag it or resize it to its proper location. This will be utilized for the dashboards.
-          </p>
-  
-          <b>Step 3: Exporting your files</b>
-          <p>There are various types of exports available. You can keep it simple by creating a PDF or PNG of your floorplan. You can also export the files necessary to generate a dashboard in Home Assistant.</p>
-          <b> Exporting a Floor Plan to Home Assistant </b>
-          <p> Start by clicking on the "export" button in the menu, and select "Export to Home Assistant". This will download a zip file containing the necessary images and config files for generating a dashboard.
-            Go to your Home Assistant software, and access the file manager. Once here, you can upload the config files which should override the currently existing files. Lastly, a lovelace dashboard file will be utilized To
-            create the dashboard in the application. </p>
-        
-        </div> 
+            {tabMode === 'step2' && 
+              <div>
+                <b>Step 2: Integrate your sensors</b>
+
+                <p> To generate a sensor menu, you must have integrated your desired sensors into your Home Assistant software. This software can generate the configuration files necessary for developing your list of sensors.
+                  A more detailed guide will come later in regards towards developing these lists and files. Once acquired, upload the files into this application and a list will be generated. </p>
+
+                <p> To place sensors, you click on the device you wish to place. This will open the device settings screen. Here, you can change the device display name, and edit which sensors you wish to focus on and view in the
+                  Home Assistant dashboard upon exporting. For example, you may have a multisensor with several types of sensors. Although, your usability only requires one of the sensors. Simply select to make that sensor active. 
+                  When creating the device, it will appear on the floorplan. You can drag it or resize it to its proper location. This will be utilized for the dashboards.
+                </p>
+              </div> 
+            }
+
+            {tabMode === 'step3' && 
+              <div>
+                <b>Step 3: Exporting your files</b>
+                <p>There are various types of exports available. You can keep it simple by creating a PDF or PNG of your floorplan. You can also export the files necessary to generate a dashboard in Home Assistant.</p>
+                <b> Exporting a Floor Plan to Home Assistant </b>
+                <p> Start by clicking on the "export" button in the menu, and select "Export to Home Assistant". This will download a zip file containing the necessary images and config files for generating a dashboard.
+                  Go to your Home Assistant software, and access the file manager. Once here, you can upload the config files which should override the currently existing files. Lastly, a lovelace dashboard file will be utilized To
+                  create the dashboard in the application. </p>
+              </div> 
+            }
+          </div> 
+        </div>
       </div>
     </div>
   );
