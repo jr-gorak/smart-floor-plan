416ceedf2986cb1c1c9cc39b177439c3930094ca
jr-gorak <jkg11@aber.ac.uk>
2025-08-04 19:49:04 +0100

    Initial dashboard exportation
    Initial logic for generating floorplan dashboard

--- src/components/menu/filemanager/ExportDropdown.js
+++ src/components/menu/filemanager/ExportDropdown.js
@@ -211,10 +211,294 @@ function ExportDropdown({canvasData, canvasState, canvasInfo, activeDropdown}) {
     zip.folder('core-registry-files').file('core.entity_registry', JSON.stringify(entityRegistry, null, 2))
   }
 
+  async function generateDashboard() {
+
+    const doorSensorIDArray = [];
+    const temperatureSensorIDArray = [];
+    const lightSensorIDArray = [];
+    for (const device in deviceList) {
+          const deviceData = deviceList[device]
+          const doorSensor = deviceData.entities.find(entity => (entity.type === "door" || entity.type === "window") && entity.visible === true)
+          const temperatureSensor = deviceData.entities.find(entity => entity.type === "temperature" && entity.visible === true)
+          const lightSensor = deviceData.entities.find(entity => entity.type.includes("light") && entity.visible === true)
+          if (doorSensor) {
+            doorSensorIDArray.push({device_id: doorSensor.device_id, id: doorSensor.id, area_id: deviceData.area_id, type: doorSensor.type})
+          } if (temperatureSensor) {
+            temperatureSensorIDArray.push({device_id: temperatureSensor.device_id, id: temperatureSensor.id, area_id: deviceData.area_id, type: temperatureSensor.type})
+          } if (lightSensor) {
+            lightSensorIDArray.push({device_id: lightSensor.device_id, id: lightSensor.id, area_id: deviceData.area_id, type: lightSensor.type})
+          }
+    }
+
+    function generateDoorEntities(data) {
+      const doorElementArray = [];
+      for (const entity of doorSensorIDArray) {
+        const doorElement = data.objects.filter(obj => obj.id === entity.device_id)
+          doorElement.forEach(obj => {
+          const closedState = {
+            type: "conditional",
+            elements: [
+              {
+                type: "state-icon",
+                entity: entity.id,
+                icon: entity.type === "door" ? "mdi:door-closed" : "mdi:window-closed-variant",
+                style: {
+                  left: Math.round((obj.left / canvasWidth) * 100) + "%",
+                  top: Math.round((obj.top / canvasHeight) * 100) + "%",
+                  transform: "scale(2,2)"
+                },
+                title: entity.type === "door" ? (entity.area_id ? `${entity.area_id} Door` : "Door") : (entity.area_id ? `${entity.area_id} Window` : "Window"),
+              }
+             ],
+            conditions: [
+              {
+                condition: "state",
+                entity: entity.id,
+                state: "Closed"
+              }
+            ],
+            title: entity.type === "door" ?  (entity.area_id ? `${entity.area_id} Door Closed` : "Door Closed") : (entity.area_id ? `${entity.area_id} Window Closed` : "Window Closed")
+          };
+          const openedState = {
+            type: "conditional",
+            elements: [
+              {
+                type: "state-icon",
+                entity: entity.id,
+                icon: entity.type === "door" ? "mdi:door-open" : "mdi:window-open-variant",
+                style: {
+                  left: (Math.round((obj.left / canvasWidth) * 100)-1) + "%",
+                  top: Math.round((obj.top / canvasHeight) * 100) + "%",
+                  transform: "scale(2,2)"
+                },
+              }
+             ],
+            conditions: [
+              {
+                condition: "state",
+                entity: entity.id,
+                state: "Open"
+              }
+            ],
+            title: entity.type === "door" ? (entity.area_id ? `${entity.area_id.replace(/^./, entity.area_id[0].toUpperCase()).replace(/_/g, " ")} Door Open` : "Door Open") : (entity.area_id ? `${entity.area_id.replace(/^./, entity.area_id[0].toUpperCase()).replace(/_/g, " ")} Window Open` : "Window Open")
+          }
+          doorElementArray.push(closedState, openedState);
+        })  
+      }
+      return doorElementArray;
+    }
+
+    function generateTemperatureEntities(data) {
+      const temperatureElementArray = [];
+      for (const entity of temperatureSensorIDArray) {
+        const temperatureElement = data.objects.filter(obj => obj.id === entity.device_id)
+          temperatureElement.forEach(obj => {
+          const temperatureObject = {
+            type: "state-badge",
+            entity: entity.id,
+            title: entity.area_id ? `${entity.area_id.replace(/^./, entity.area_id[0].toUpperCase()).replace(/_/g, " ")}` : "No Room",
+            style: {
+              left: (Math.round((obj.left / canvasWidth) * 100)-1) + "%",
+              top: Math.round((obj.top / canvasHeight) * 100) + "%",
+            },
+          };
+          temperatureElementArray.push(temperatureObject);
+        })  
+      }
+      return temperatureElementArray;
+    }
+
+    function generateLightEntities(data) {
+      const lightElementArray = [];
+      for (const entity of lightSensorIDArray) {
+        const lightElement = data.objects.filter(obj => obj.id === entity.device_id)
+          lightElement.forEach(obj => {
+          const onState = {
+            type: "conditional",
+            title: entity.area_id ? `${entity.area_id.replace(/^./, entity.area_id[0].toUpperCase()).replace(/_/g, " ")} light on` : "Light on",
+            conditions: [
+              {
+                condition: "numeric_state",
+                entity: entity.id,
+                above: 200
+              }
+            ],
+            elements: [
+              {
+                type: "state-icon",
+                entity: entity.id,
+                icon: "mdi:lightbulb-on",
+                style: {
+                  left: Math.round((obj.left / canvasWidth) * 100) + "%",
+                  top: Math.round((obj.top / canvasHeight) * 100) + "%",
+                  transform: "scale(2,2)"
+                },
+              }
+            ],
+          };
+          const offState = {
+            type: "conditional",
+            title: entity.area_id ? `${entity.area_id.replace(/^./, entity.area_id[0].toUpperCase()).replace(/_/g, " ")} light off` : "Light off",
+            conditions: [
+              {
+                condition: "numeric_state",
+                entity: entity.id,
+                below: 200
+              }
+            ],
+            elements: [
+              {
+                type: "state-icon",
+                entity: entity.id,
+                icon: "mdi:lightbulb-outline",
+                style: {
+                  left: Math.round((obj.left / canvasWidth) * 100) + "%",
+                  top: Math.round((obj.top / canvasHeight) * 100) + "%",
+                  transform: "scale(2,2)"
+                },
+              }
+            ],
+          }
+          lightElementArray.push(onState, offState);
+        })  
+      }
+      return lightElementArray;
+    }
+
+    function generateFloorDashboard() {
+      
+      const floorDashboardArray = []
+      for (const key in floorData) {
+        const data = floorData[key];
+        const floorDashboard = {
+          type: "vertical-stack",
+          cards:  [
+            {
+            type: "markdown",
+            content: "# Select a floor plan\n"
+            },
+            {
+              type: "horizontal-stack",
+              cards: [
+                {
+                  type: "button",
+                  name: "Temperature",
+                  show_name: false,
+                  show_icon: true,
+                  entity: "input_boolean.show_celsius_table",
+                  icon: "mdi:temperature_celsius",
+                  show_state: false,
+                  tap_action: {
+                    action: "call-service",
+                    service: "script.only_celsius_table"
+                  }
+                },
+                {
+                  type: "button",
+                  name: "Doors",
+                  show_name: false,
+                  show_icon: true,
+                  entity: "input_boolean.show_doors_table",
+                  icon: "mdi:door",
+                  show_state: false,
+                  tap_action: {
+                  action: "call-service",
+                  service: "script.only_doors_table"
+                  }
+                },
+                {
+                  type: "button",
+                  name: "Lamps",
+                  show_name: false,
+                  show_icon: true,
+                  entity: "input_boolean.show_lamps_table",
+                  icon: "mdi:lightbulb",
+                  show_state: false,
+                  tap_action: {
+                    action: "call-service",
+                    service: "script.only_lamps_table"
+                  }
+                }
+              ]
+            },
+            {
+              type: "grid",
+              columns: 1,
+              cards: [
+                {
+                  type: "picture-elements",
+                  title: "Doors",
+                  visibility: [
+                    {
+                      condition: null,
+                      entity: "input_boolean.show_doors_table",
+                      state: "on"
+                    }
+                  ],
+                  elements: generateDoorEntities(data),
+                  image: "imgURLhere"
+                },
+                {
+                  type: "picture-elements",
+                  title: "Temperature",
+                  visibility: [
+                    {
+                      condition: null,
+                      entity: "input_boolean.show_celsius_table",
+                      state: "on"
+                    }
+                  ],
+                  elements: generateTemperatureEntities(data),
+                  image: "imgURLhere"
+                }, 
+                {
+                  type: "picture-elements",
+                  title: "Lights",
+                  visibility: [
+                    {
+                      condition: null,
+                      entity: "input_boolean.show_lamps_table",
+                      state: "on"
+                    }
+                  ],
+                  elements: generateLightEntities(data),
+                  image: "imgURLhere"
+                }, 
+              ]
+            }
+          ]
+        }
+        floorDashboardArray.push(floorDashboard)
+      }
+      return floorDashboardArray
+    }
+
+    const lovelaceDashboardExport = {
+      version: 1,
+      minor_verson: 1,
+      key: "lovelace.smart_home",
+      data: {
+        config: {
+          views: [
+            {
+              title: "Floorplans",
+              cards: generateFloorDashboard()
+            },
+            {
+              //More dashboards here
+            }
+          ]
+        }
+      }
+    }
+    zip.file('lovelace.smart_home', JSON.stringify(lovelaceDashboardExport, null, 2))
+  }
+
   async function exportData() {
 
     await generateImages("home-assistant");
     await generateJsonFiles();
+    await generateDashboard();
 
     await zip.generateAsync({type: 'blob'}).then((blob) => {
         saveAs(blob, `${canvasName.toLowerCase().replace(/ /g, '_')}_export.zip`)
@@ -223,9 +507,6 @@ function ExportDropdown({canvasData, canvasState, canvasInfo, activeDropdown}) {
 
 
   }
-
-  console.log(deviceRegistry)
-  console.log(entityRegistry)
 
   return (
     <div>
