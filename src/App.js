6479e55310b763a60895d8d82f7bfeff3b41fe8c
jr-gorak <jkg11@aber.ac.uk>
2025-08-17 00:10:47 +0100

    Prevent native scrolling
    Added an event listener to prevent native pinch scrolling on touch. This is to ensure that the user uses the built-in zooming logic.

--- src/App.js
+++ src/App.js
@@ -154,6 +154,11 @@ function App() {
   }
 
   window.addEventListener("beforeunload", sessionSave);
+  document.addEventListener('touchmove', (e) => {
+    if (e.touches.length > 1) {
+      e.preventDefault();
+    }
+  }, { passive: false })
 
 
   return (
